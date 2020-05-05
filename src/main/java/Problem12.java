2e48f21f0c75adff9ea7f4e7d23df1ce1a7bdafd
DenisPrivistirescu <denis.privistirescu@ness.com>
2020-05-05 18:41:22 +0300

    #Added problem11 and 12 for homework part1

--- src/main/java/Problem12.java
+++ src/main/java/Problem12.java
@@ -3,16 +3,38 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Problem12 {
+
+    public static double Average(int notes[], int max){
+        int sum = 0;
+        double average = 0.0;
+        for(int i= 1; i<max; i++){
+            sum = sum + notes[i];
+            average = (double)sum/(double)i;
+
+        }
+        return average;
+    }
     public static void main(String[] args) {
 
-        String quit = "quit";
-            Scanner input = new Scanner(System.in);
-            System.out.println("Enter 20 grades:");
-            for (int i = 0; i < 19; i++) {
-                int[] array = new int[i];
-                array[i] = input.nextInt();
-                System.out.println(Arrays.toString(array));
+        Scanner input = new Scanner(System.in);
+        int[] notes = new int[20];
+        int sum = 0;
+        System.out.println("Enter 20 grades: ");
 
+            for (int i = 1; i < 21; i++) {
+                notes[i] = input.nextInt();
+                if (notes[i] < 0) {
+                    System.out.println("This is not a grade.");
+                }
+                if (notes[i] > 10) {
+                    System.out.println("This is not a grade.");
+                }
+                if (notes[i] == -1) {
+                    break;
+                }
+                System.out.println(Average(notes, i));
             }
+        }
+
     }
-}
+
