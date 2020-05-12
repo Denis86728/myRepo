348ffb431b5b06aad6a7057e6b43f0e8d38ee55e
DenisPrivistirescu <denis.privistirescu@ness.com>
2020-05-12 11:43:27 +0300

    #Problem 12 updated

--- src/main/java/Problem12.java
+++ src/main/java/Problem12.java
@@ -4,36 +4,38 @@ import java.util.Scanner;
 
 public class Problem12 {
 
-    public static double Average(int notes[], int max){
+    public static double Average(int notes[], int noOfGrades){
         int sum = 0;
         double average = 0.0;
-        for(int i= 1; i<max; i++){
+        for(int i= 1; i < notes.length; i++){
             sum = sum + notes[i];
-            average = (double)sum/(double)i;
-
         }
+        average = (double)sum/(double)noOfGrades;
         return average;
     }
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
         int[] notes = new int[20];
-        int sum = 0;
+        int noOfGrades= 0;
+
         System.out.println("Enter 20 grades: ");
 
-            for (int i = 1; i < 21; i++) {
-                notes[i] = input.nextInt();
-                if (notes[i] < 0) {
-                    System.out.println("This is not a grade.");
-                }
-                if (notes[i] > 10) {
-                    System.out.println("This is not a grade.");
-                }
-                if (notes[i] == -1) {
-                    break;
-                }
-                System.out.println(Average(notes, i));
+        for(int i = 0; i < 20; i++){
+            notes[i] = input.nextInt();
+            if(notes[i] < 0 || notes[i] > 10){
+                System.out.println("This is not a grade");
             }
+            else
+            {
+                noOfGrades++;
+            }
+            if(notes[i] == -1){
+                break;
+            }
+        }
+        System.out.printf("The average is: " + Average(notes,noOfGrades));
         }
 
     }
